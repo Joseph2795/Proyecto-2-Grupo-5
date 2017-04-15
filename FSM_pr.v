f90c2096e7e1bdf40502ff5f9558a40e2bb13321
eshidalgo <aestebanhr25@gmail.com>
2017-04-15 17:46:22 -0600

    Add files via upload

--- FSM_pr.v
+++ FSM_pr.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module FSM_pr(date, stime, timer, clk, control, reset, counter, actuals);
+module FSM_pr(date, stime, timer, clk, control, reset, counter, actuals,datos,AD,contador,ram);
 
     parameter stnd = 4'b0000;     
     parameter read = 4'b0001;
@@ -11,11 +11,16 @@ module FSM_pr(date, stime, timer, clk, control, reset, counter, actuals);
     parameter read4 = 4'b0111;      
     parameter formato = 4'b1000; 
     
-    input date, stime, timer,  clk, reset;
-    output control, counter, actuals;
+    input date, stime, timer,  clk, reset,datos;
+    output control, counter, actuals,AD,contador,ram;
     reg [3:0] actuals = stnd;
     reg [3:0] control;
     reg [5:0] counter;
+    reg [7:0] contador;
+    reg [7:0] AD;
+    reg [7:0] ram;
+    wire [7:0] datos;
+    
     wire orstate;
     assign orstate = date | stime | timer;       
    /*Control
@@ -42,6 +47,25 @@ begin
         end
     end
 end 
+
+always @(posedge clk)
+   if (reset) 
+   begin
+       contador = 7'b00100001;
+   end
+   else if (counter == 0 && orstate == 0) begin
+       if (contador < 67) begin
+           if (contador == 38) begin
+               contador = 65;
+           end
+           else begin
+               contador = contador + 1;
+           end
+       end
+       else begin
+           contador = 33;
+       end
+   end       
 
 always @(posedge clk)
     begin 
@@ -120,5 +144,26 @@ always @(posedge clk)
             end
         endcase
      end
+           
+always@(posedge clk)
+    if  (counter >= 10 && counter <= 16 && orstate==0)
+        begin 
+           assign AD = contador; 
+        end
+    else if (counter >=17 && counter <= 23)
+        begin
+            assign ram = contador;
+        end    
+    else if (counter >= 30 && counter <= 36)
+        begin
+            assign AD = datos; 
+        end
+    else if (counter >=37 && counter <= 40)
+        begin
+            assign ram = datos;
+        end    
+    else
+        assign AD = 0;                    
+
 endmodule
 
